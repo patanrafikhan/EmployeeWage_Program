a83067346ad66535df678c85000975ff10e0eb89
patanrafikhan <iamkhan342@gmail.com>
2023-01-28 13:49:09 +0530

    Added : UC1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Employeewage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Employee Wage App");
+           
+            Random ren =new Random();
+            int n=ren.Next();
+            if (n == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
